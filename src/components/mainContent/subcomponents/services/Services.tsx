4ef8b269d30ac0c52d8e44ec06619952048b7d0f
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2024-03-14 13:58:36 -0500

    chore: modificar componentes about y service

--- src/components/mainContent/subcomponents/services/Services.tsx
+++ src/components/mainContent/subcomponents/services/Services.tsx
@@ -3,27 +3,29 @@ import styles from './Services.module.css';
 export function Services() {
     return (
         <div className={styles.card}>
-            <h2>Servicios</h2>
-            <div>
-                <p>
-                    El Semillero de Investigación de la Universidad Nacional del Santa (SISUNS) ofrece una variedad de servicios para estudiantes, profesionales y la comunidad en general. Nuestro objetivo es promover la investigación y la innovación en el campo de la ingeniería de sistemas e informática. Algunos de nuestros servicios incluyen:
-                </p>
-            </div>
-            <div>
-                <ul>
-                    <li>Asesoría en proyectos de investigación.</li>
-                    <li>Capacitaciones y talleres en temas de tecnología y desarrollo de software.</li>
-                    <li>Desarrollo de soluciones tecnológicas innovadoras.</li>
-                    <li>Participación en proyectos de investigación aplicada.</li>
-                    <li>Colaboración con la industria en proyectos de innovación y desarrollo tecnológico.</li>
+            <h2 className={styles.cardTitle}>Servicios</h2>
+            <div className={styles.cardSection}>
+                <h3 className={styles.sectionTitle}>Nuestros Servicios</h3>
+                <ul className={styles.serviceList}>
+                    <li className={styles.serviceItem}>Asesoría en proyectos de investigación.</li>
+                    <li className={styles.serviceItem}>Capacitaciones y talleres en temas de tecnología y desarrollo de
+                        software.
+                    </li>
+                    <li className={styles.serviceItem}>Desarrollo de soluciones tecnológicas innovadoras.</li>
+                    <li className={styles.serviceItem}>Participación en proyectos de investigación aplicada.</li>
+                    <li className={styles.serviceItem}>Colaboración con la industria en proyectos de innovación y
+                        desarrollo tecnológico.
+                    </li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.cardSection}>
+                <h3 className={styles.sectionTitle}>Nuestro Compromiso</h3>
                 <p>
-                    Nuestros servicios están diseñados para apoyar el crecimiento y la formación de profesionales en el campo de la ingeniería de sistemas e informática. Buscamos promover la investigación, el desarrollo tecnológico y la innovación en nuestra comunidad universitaria y más allá.
+                    Nuestros servicios están diseñados para apoyar el crecimiento y la formación de profesionales en el
+                    campo de la ingeniería de sistemas e informática. Buscamos promover la investigación, el desarrollo
+                    tecnológico y la innovación en nuestra comunidad universitaria y más allá.
                 </p>
             </div>
-
         </div>
     );
 }
